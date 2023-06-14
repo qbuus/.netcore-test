6aa709fb484ceff0a4243553c41c9c509fb15d9f
qbuus <jkocz867@gmail.com>
2023-06-14 13:27:36 +0200

    roles seeder

--- RestaurantSeedercs.cs
+++ RestaurantSeedercs.cs
@@ -12,12 +12,38 @@ namespace API.entityFramework
         {
             if(_dbContext.Database.CanConnect())
             {
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+
                 if(!_dbContext.Type.Any())
                 {
                     var type = GetTypes();
                     _dbContext.Type.AddRange(type);
+                    _dbContext.SaveChanges();
                 }
             }
+        }
+        
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                }, new Role()
+                {
+                    Name = "Admin"
+                },new Role()
+                {
+                    Name = "Manager"
+                }
+            };
+            return roles;
         }
 
         private IEnumerable<TestApiClassEntityFramework> GetTypes()
